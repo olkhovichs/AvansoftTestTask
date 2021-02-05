f1c7fa0a82380a4aa2147f37e14b8b758dab6ffb
Olkhovich Semyon <olkhovichs@icloud.com>
2021-02-05 21:27:20 +0300

    car: fixed catalog size

--- src/car.cpp
+++ src/car.cpp
@@ -22,7 +22,7 @@ std::vector<Car> Car::setCatalog() {
 	std::vector<Car> catalog;
 
 	srand(time(nullptr));
-	for (int i = 1; i <= 11; i++) {
+	for (int i = 1; i <= 10; i++) {
 		carPrice = 1.10 + (double)(rand()) / RAND_MAX * (6.49 - 1.10);
 		color = carColors[rand() % carColors.size()];
 		carSpeed = 220 + (double)(rand()) / RAND_MAX * (340 - 220);
@@ -34,7 +34,7 @@ std::vector<Car> Car::setCatalog() {
 
 void Car::printCatalog(std::vector<Car> catalog, double filterPrice, double filterSpeed) {
 	std::cout << "| ID |  Price  |  Color  | Max speed |" << std::endl;
-	for (int i = 0; i < catalog.size() - 1; i++) {
+	for (int i = 0; i < catalog.size(); i++) {
 		if (catalog[i].price <= filterPrice && catalog[i].speed >= filterSpeed) {
 			std::cout << "-----------------------------------" << std::endl;
 			std::cout << std::setw(3) << catalog[i].id
